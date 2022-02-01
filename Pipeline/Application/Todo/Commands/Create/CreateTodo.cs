9db19a7ff356585a11ae546f2c020b929d4eb8ba
sl-yannterraillon <yann.terraillon@groupeseloger.com>
2022-02-01 16:18:57 +0100

    cleaning

--- Pipeline/Application/Todo/Commands/Create/CreateTodo.cs
+++ Pipeline/Application/Todo/Commands/Create/CreateTodo.cs
@@ -5,8 +5,6 @@ public static class CreateTodo
 {
     public record Request(string Name) : IRequest<ApiResponse<string>>;
 
-    public record Response(string Id);
-
     public class Handler : IRequestHandler<Request, ApiResponse<string>>
     {
         public Task<ApiResponse<string>> Handle(Request request, CancellationToken cancellationToken)
